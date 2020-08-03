3992f8efbb60d55a336c696d654aa3ad47e4d63e
EmmanuelSage <xyemmanuel@gmail.com>
2020-08-03 22:17:52 +0100

    complete parser for mypl

--- mypl.js
+++ mypl.js
@@ -5,15 +5,70 @@ function id(x) { return x[0]; }
 var grammar = {
     Lexer: undefined,
     ParserRules: [
-    {"name": "program", "symbols": ["var_assignment"], "postprocess": id},
-    {"name": "program", "symbols": ["number"], "postprocess": id},
+    {"name": "program", "symbols": ["statements"], "postprocess": id},
+    {"name": "statements", "symbols": ["_", "statement", "_"], "postprocess": 
+        data => [data[1]]
+          },
+    {"name": "statements", "symbols": ["_", "statement", "_", {"literal":"\n"}, "statements"], "postprocess": 
+        data => [data[1], ...data[4]]
+          },
+    {"name": "statement", "symbols": ["var_assignment"], "postprocess": id},
+    {"name": "statement", "symbols": ["print_statement"], "postprocess": id},
+    {"name": "statement", "symbols": ["while_loop"], "postprocess": id},
+    {"name": "while_loop$string$1", "symbols": [{"literal":"w"}, {"literal":"h"}, {"literal":"i"}, {"literal":"l"}, {"literal":"e"}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "while_loop", "symbols": ["while_loop$string$1", "__", "binary_expression", "_", {"literal":"["}, "_", {"literal":"\n"}, "statements", {"literal":"\n"}, "_", {"literal":"]"}], "postprocess": 
+        data => {
+          return {
+            type: "while_loop",
+            condition: data[2],
+            body: data[7]
+          }
+        }
+          },
+    {"name": "print_statement$string$1", "symbols": [{"literal":"p"}, {"literal":"r"}, {"literal":"i"}, {"literal":"n"}, {"literal":"t"}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "print_statement", "symbols": ["print_statement$string$1", "__", "expression"], "postprocess": 
+        data => {
+          return {
+            type: "print_statement",
+            expression: data[2]
+          }
+        }
+          },
+    {"name": "expression", "symbols": ["unary_expression"], "postprocess": id},
+    {"name": "expression", "symbols": ["binary_expression"], "postprocess": id},
+    {"name": "unary_expression", "symbols": ["number"], "postprocess": id},
+    {"name": "unary_expression", "symbols": ["identifier"], "postprocess": id},
+    {"name": "binary_expression", "symbols": ["unary_expression", "_", "operator", "_", "expression"], "postprocess": 
+        data => {
+          return {
+            type: "binary_expression",
+            left: data[0],
+            operator: data[2],
+            right: data[4]
+          }
+        }
+          },
+    {"name": "operator", "symbols": [{"literal":"+"}], "postprocess": id},
+    {"name": "operator", "symbols": [{"literal":"-"}], "postprocess": id},
+    {"name": "operator", "symbols": [{"literal":"*"}], "postprocess": id},
+    {"name": "operator", "symbols": [{"literal":"/"}], "postprocess": id},
+    {"name": "operator", "symbols": [{"literal":">"}], "postprocess": id},
+    {"name": "operator", "symbols": [{"literal":"<"}], "postprocess": id},
+    {"name": "operator$string$1", "symbols": [{"literal":">"}, {"literal":"="}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "operator", "symbols": ["operator$string$1"], "postprocess": id},
+    {"name": "operator$string$2", "symbols": [{"literal":"<"}, {"literal":"="}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "operator", "symbols": ["operator$string$2"], "postprocess": id},
+    {"name": "operator$string$3", "symbols": [{"literal":"="}, {"literal":"="}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "operator", "symbols": ["operator$string$3"], "postprocess": id},
+    {"name": "operator$string$4", "symbols": [{"literal":"="}, {"literal":"="}, {"literal":"="}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "operator", "symbols": ["operator$string$4"], "postprocess": id},
     {"name": "var_assignment$string$1", "symbols": [{"literal":":"}, {"literal":"="}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "var_assignment", "symbols": ["identifier", "var_assignment$string$1", "number"], "postprocess": 
+    {"name": "var_assignment", "symbols": ["identifier", "_", "var_assignment$string$1", "_", "expression"], "postprocess": 
         data => {
           return {
             type: "var_assignment",
             varname: data[0].join(""),
-            value: data[2]
+            value: data[4]
           }
         }
           },
@@ -30,7 +85,13 @@ var grammar = {
     {"name": "digits$ebnf$1", "symbols": ["digits$ebnf$1", /[0-9]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "digits", "symbols": ["digits$ebnf$1"], "postprocess": 
         data => data[0].join("")
-          }
+          },
+    {"name": "_$ebnf$1", "symbols": []},
+    {"name": "_$ebnf$1", "symbols": ["_$ebnf$1", /[ ]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "_", "symbols": ["_$ebnf$1"]},
+    {"name": "__$ebnf$1", "symbols": [/[ ]/]},
+    {"name": "__$ebnf$1", "symbols": ["__$ebnf$1", /[ ]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "__", "symbols": ["__$ebnf$1"]}
 ]
   , ParserStart: "program"
 }
--- parser.js
+++ parser.js
@@ -1,11 +1,26 @@
 const nearley = require('nearley')
 const grammar = require('./mypl.js')
+const fs = require('mz/fs')
+const path = require('path')
 
-const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar))
 
-try {
-  parser.feed('as:=12.89')
-  console.log('parser succeded : ', parser.results)
-} catch (error) {
-  console.log('parser failed : ', error.message)
+async function main() {
+
+  const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar))
+  const filename = process.argv[2]
+  const outputFilename = path.basename(filename, '.mypl') + ".ast"
+  const code = (await fs.readFile(filename)).toString()
+  
+  try {
+    parser.feed(code)
+    const ast = parser.results[0]
+    await fs.writeFile(outputFilename, JSON.stringify(ast, null, 2))
+    console.log('parser succeded')
+    console.log('wrote ', outputFilename)
+  } catch (error) {
+    console.log('parser failed : ', error.message)
+  }
+
 }
+
+main()
